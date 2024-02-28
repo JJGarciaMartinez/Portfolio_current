bb7df9a05c5059f3dac8988fbb07df7d576ed542
Juan5 <juangm2302@gmail.com>
2024-02-28 11:33:32 -0600

    FinishFirstVersion

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+export const plugins = {
+  "postcss-import": {},
+  "tailwindcss/nesting": {},
+  tailwindcss: {},
+  autoprefixer: {},
 };
