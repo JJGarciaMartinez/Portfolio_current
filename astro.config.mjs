417ba6fc639bc893cef636a9c65c9e6cb9defe13
Juan5 <juangm2302@gmail.com>
2024-04-06 14:47:52 -0600

    V1.501 Add Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,12 @@ export default defineConfig({
     }),
     react(),
   ],
+
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
